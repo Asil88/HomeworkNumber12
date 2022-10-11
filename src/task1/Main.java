59fd9a6774cf5494960bfe64e3ffb699a58909da
Anton Lisovski <lisovskijj.99@gmail.com>
2022-10-11 17:44:28 +0300

    task 1

--- src/task1/Main.java
+++ src/task1/Main.java
@@ -11,31 +11,50 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Path path = Path.of(scanner.nextLine());
-
         try {
             Reader.readDoc(path);
-
-        } catch (IOException e) {
+        } catch (IOException | ValidException e) {
             System.out.println(e.getMessage());
         }
     }
 }
 class Reader {
 
-    public static void readDoc(Path path) throws IOException {
-
+    public static void readDoc(Path path) throws ValidException, IOException {
         List<String> strings = Files.readAllLines(path);
+        BufferedWriter validWriter = Files.newBufferedWriter(Path.of("ValidStrings.txt"));
+        BufferedWriter invalidWriter = Files.newBufferedWriter(Path.of("InvalidStrings.txt"));
+        try {
+            for (String s : strings) {
+                if (s.length() == 15 && (s.startsWith("docnum") || s.startsWith("contract"))) {
+                    validWriter.write(s + "\n");
+                }
+            }
+        } catch (ValidException | IOException e) {
+            throw new ValidException();
 
-        for (String s : strings) {
-            if (s.length() == 15 && (s.startsWith("docnum") || s.startsWith("contract"))) {
-                BufferedWriter validWriter = Files.newBufferedWriter(Path.of("ValidStrings.txt"));
-                validWriter.write(s);
-
-            } else {
-                BufferedWriter invalidWriter = Files.newBufferedWriter(Path.of("InvalidStrings.txt"));
-                invalidWriter.write(s);
+        } finally {
+            validWriter.close();
+        }
+        try {
+            for (String s : strings) {
+                if (s.length() != 15 && (!s.startsWith("docnum") || !s.startsWith("contract"))) {
+                    invalidWriter.write(s + "\n");
+                }
+            }
+        } catch (ValidException | IOException e) {
+            System.out.println(e.getMessage());
+            throw new ValidException(e);
+        } finally {
+            invalidWriter.close();
+        }
+    }
+}
+    class Condition {
+        public static void validationTrue(String s) throws ValidException {
+            if (s.length() != 15 && (!s.startsWith("docnum") || !s.startsWith("contract"))) {
+                throw new ValidException("Номер " + s + " дожен начинаться c docnum или contract и состоять из 15 символов");
             }
         }
     }
 
-}
